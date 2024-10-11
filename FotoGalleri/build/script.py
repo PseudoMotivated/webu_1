d09732af773962a7c7f3d56d53633f55fc99dedf
PseudoMotivated <122965701+PseudoMotivated@users.noreply.github.com>
2024-10-11 21:04:44 +0200

    Fixed validation error

--- FotoGalleri/build/script.py
+++ FotoGalleri/build/script.py
@@ -2,4 +2,4 @@ import os
 
 for entry in os.scandir("C:\\Users\\eabdawa001\\Documents\\GitHub\\webu_1\\FotoGalleri\\Compressed"):      
     
-     print("<img class=\"img-entry\" src=\"Compressed/" + entry.name + "\"> \n <div class=\"filler\"></div>")
+     print("<img alt=\"Im not captioning all that\" class=\"img-entry\" src=\"Compressed/" + entry.name + "\"> \n <div class=\"filler\"></div>")
